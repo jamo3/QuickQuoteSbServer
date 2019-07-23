1f1ee7e9b8e005fb29a7a3081f7baf88c05cdaba
Jay Moss <jay.moss@polarisalpha.com>
2019-07-23 09:23:42 -0600

    improve repo tests

--- src/test/java/com/perfatech/qq/repository/RateRepoTest.java
+++ src/test/java/com/perfatech/qq/repository/RateRepoTest.java
@@ -7,12 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.number.BigDecimalCloseTo.closeTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
@@ -26,22 +30,34 @@ public class RateRepoTest
     public void findByUnitName()
     {
         // test data added via changelog file  qq-db-V0.0.1.xml
-        List<Rate> rates = rateRepo.findByUnitNameOrderByBeginDate("unit-1");
-        assertFalse(rates.isEmpty());
+        List<Rate> ratesForUnit1 = rateRepo.findByUnitNameOrderByBeginDate("unit-1");
+        assertFalse(ratesForUnit1.isEmpty());
+
+        Rate rate = ratesForUnit1.get(0);
+        assertEquals("rate-1.1", rate.getName());
+        assertEquals(Integer.valueOf(1), rate.getId());
+
+        assertEquals(LocalDate.parse("2019-01-01"), rate.getBeginDate());
+        assertEquals( LocalDate.parse("2019-02-28"), rate.getEndDate());
+        assertEquals(Integer.valueOf(5), rate.getMinimumStay());
+
+        BigDecimal price = new BigDecimal(99.95);
+        BigDecimal priceDelta = new BigDecimal(0.001);
+        assertThat(rate.getPrice(), is(closeTo(price, priceDelta)));
     }
 
     @Test
     public void getPriceForDate()
     {
         // test data added via changelog file  qq-db-V0.0.1.xml
-        String date = "2019-02-17";  //default, ISO_LOCAL_DATE
+        String date = "2019-07-17";  //default, ISO_LOCAL_DATE
         LocalDate rentalDate = LocalDate.parse(date);
 
         Optional<Rate> optRate = rateRepo.findByBeginDateBeforeAndEndDateAfterAndUnitName(
             rentalDate, rentalDate, "unit-1");
         assertTrue(optRate.isPresent());
         Rate rate = optRate.get();
-        assertEquals("99.95", rate.getPrice().toString());
-        assertEquals(5, (int) rate.getMinimumStay());
+        assertEquals("400.00", rate.getPrice().toString());
+        assertEquals(4, (int) rate.getMinimumStay());
     }
 }
--- src/test/java/com/perfatech/qq/repository/UnitRepoTest.java
+++ src/test/java/com/perfatech/qq/repository/UnitRepoTest.java
@@ -7,9 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.number.BigDecimalCloseTo.closeTo;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
@@ -25,6 +30,19 @@ public class UnitRepoTest
         // test data added via changelog file  qq-db-V0.0.1.xml
         Optional<Unit> unit = unitRepo.findByName("unit-1");
         assertTrue(unit.isPresent());
+
+        Unit unit1 = unit.get();
+        assertEquals("unit-1", unit1.getName());
+
+        BigDecimal taxRate = new BigDecimal(0.2345);
+        BigDecimal taxRateDelta = new BigDecimal(0.00001);
+        assertThat(unit1.getTaxRate(), is(closeTo(taxRate, taxRateDelta)));
+
+        BigDecimal cleaningFee = new BigDecimal(123.45);
+        BigDecimal cleaningFeeDelta = new BigDecimal(0.001);
+        assertThat(unit1.getCleaningFee(), is(closeTo(cleaningFee, cleaningFeeDelta)));
+
+        assertEquals("3456", unit1.getCalendarId());
 
         unit = unitRepo.findByName("does-not-exist");
         assertFalse(unit.isPresent());
